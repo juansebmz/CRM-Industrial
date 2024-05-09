8a4ede9bccd68f5d87a65c6ab0e4db1d8bfac911
Gabriel Padilla <padillabustosduvergabriel@gmail.com>
2024-05-08 20:15:38 -0500

    agrego gabriel

--- src/components/products.js
+++ src/components/products.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Appbar from './Appbar';
 
 
-function products() {
 
+function products() {
+  const titleStyle = {
+    textAlign: 'left ', 
+    fontSize: '1.8em', 
+    fontWeight: 'regular', 
+    color: '#000', 
+    padding: '20px 0',
+    paddingLeft: '73px',
+ 
+  };
 
   return (
     <div style={{ backgroundColor: 'white' }}>
       <Appbar/>
-      <div>productos</div>
+      <div style={titleStyle}> Productos </div>
     </div>
   );
 }
